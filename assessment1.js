e51cbb7d5ea9cc56cab247c903ffb020416cc692
COR Students <corstudents@Matts-MBP-77.localdomain>
2018-10-22 12:25:14 -0400

    finished project

---
+++ assessment1.js
@@ -0,0 +1,42 @@
+/*Question 1: Create a variable of each of the following  data types String, Int, Bool, Float*/
+ let name = 'Crystal'; //string
+ let num = 25; //integer
+ let trueOrFalse = true;//bool
+ let pi = 3.14;//float
+
+/*Question 2: Create a variable called trueOrFalse. Create an if statement so that if the variable is true the number 1 
+is returned. Otherwise the number 2 is returned.*/
+Answer:
+   let trueOrFalse= 'true';
+       if(trueOrFalse === '1'){
+          return '1'
+  } else if (trueOrFalse === '2'){
+          return '2'
+  }
+  console.log(trueOrFalse='1' || '2')
+
+/*Question 3: Create a for loop that counts every even number from 1-100 starting at 0*/
+  for(i=0; i<=100; i+=2){
+     console.log(i);
+   }
+         
+
+/*Question 4: Create a variable that is a bool and is equal to true. Create a while loop that will print "hello" to the 
+console 20 times and then change that variable to false ending the loop.*/
+   let trueOrFalse = "true"
+   while(trueOrFalse < 20){
+    trueOrFalse++
+    
+    }
+    console.log("hello")
+    trueOrFalse='false'
+   
+  
+
+/*Question 5: Create a function that can determine if a parameter is true or false. If it is true return the string
+"true". Otherwise, return the string "false".*/
+
+ 
+
+/*Bonus Question: What does D.R.Y stand for?*/
+//Answer: Don't repeat yourself
